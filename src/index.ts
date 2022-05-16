318de67b312b75cb8db9759cafc3ec4057416dc6
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-15 21:35:53 -0300

    soloConsola

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,22 @@
-//++++++usar para HTML ++++++
-let nombreAlumno = document.getElementById("nombreAlumno");
-let notaPractica = document.getElementById("notaPractica");
-let notaProblemas = document.getElementById("notaProblemas");
-let notaTeorica = document.getElementById("notaTeorica");
-let btnCalcular = document.getElementById("btnCalcular");
+
 // ++++++++++ usar para consola +++++
-//let notaPractica: number = 0;
-//let notaProblemas: number = 10;
-//let notaTeorica: number = 10;
+let nombreAlumno: string = "Jose"
+let notaPractica: number = 8;
+let notaProblemas: number = 10;
+let notaTeorica: number = 10;
+let notaPracticaFinal: number = 0
+let notaProblemasFinal: number = 0 
+let notaTeoricaFinal: number = 0
 
-btnCalcular.addEventListener("click", () => {
-let notaPracticaFinal: number = 10 * Number(notaPractica.value) / 100;
-let notaProblemasFinal: number = 50 * Number(notaProblemas.value) / 100;
-let notaTeoricaFinal: number = 40 * Number(notaTeorica.value) / 100;
-
+console.log(nombreAlumno)
 if (notaPractica <= 10) {
-  //notaPracticaFinal = (10 * notaPractica) / 100;
+  notaPracticaFinal = (10 * notaPractica) / 100;
   console.log("Su nota practica es: ", notaPracticaFinal);
-}
-if (notaProblemas <= 10) {
-  //notaProblemasFinal = (50 * notaProblemas) / 100;
+} if (notaProblemas <= 10) {
+  notaProblemasFinal = (50 * notaProblemas) / 100;
   console.log("Su nota de problemas es: ", notaProblemasFinal);
-}
-if (notaTeorica <= 10) {
-  //notaTeoricaFinal = (40 * notaTeorica) / 100;
+} if (notaTeorica <= 10) {
+  notaTeoricaFinal = (40 * notaTeorica) / 100;
   console.log("Su nota Teorica es: ", notaTeoricaFinal);
 } else {
   console.log("Error");
@@ -32,4 +25,3 @@ if (notaTeorica <= 10) {
 let notaTotal: number =
   notaPracticaFinal + notaProblemasFinal + notaTeoricaFinal;
 console.log("Su calificacion final es", notaTotal);
- }
